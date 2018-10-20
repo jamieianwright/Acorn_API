fd8f06f835a7f75e3dabac835c809d416012875e
jamie wright <https://jamieianwright@bitbucket.org/jamieianwright/>
2018-10-20 19:23:07 +0100

    added GET by ID, POST & PUT routes

--- routes/supplier_router.js
+++ routes/supplier_router.js
@@ -2,10 +2,29 @@ var express = require("express");
 var router = express.Router({mergeParams: true});
 const knex = require('knex')(require('../knexfile')); 
 
-router.get("/", function(req, res) {
+router.get("/", (req, res) => {
     knex('supplier').then(suppliers => {
         res.json(suppliers)
     })
 });
+
+router.get("/:id", (req, res) => {
+    knex('supplier').where('id', req.params.id).then(suppliers => {
+        res.json(suppliers)
+    })
+})
+
+router.post("/", (req, res) => {
+    knex('supplier').insert(req.body).then( result => {
+        res.send(result)
+    })
+})
+
+router.put('/:id', (req, res) => {
+    knex('supplier').update(req.body).then( result => {
+        res.send(result)
+    })
+})
+
 
 module.exports = router;
