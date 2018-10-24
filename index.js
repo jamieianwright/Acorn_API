5ea4640bef345a16a09dfb3cd32239bc46cfb516
jamie wright <https://jamieianwright@bitbucket.org/jamieianwright/>
2018-10-24 08:21:03 +0100

    Solved cors issue with nom package 'cors'

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 var SuppliersRoutes = require("./routes/supplier_router");
+const cors = require('cors')
 
 const app = express()
-
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}));
 
