7e09c6b2e4b76a44ed4a5da335bb00b7989f4ee9
cjlovescoffee <cjlovescoffee@gmail.com>
2018-10-25 14:33:32 +0100

    Update .env and knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 module.exports = {
     client: 'mysql',
     connection: {
-      host: process.env.HOST,
-      user: process.env.USER,
-      password: process.env.PASSWORD,
-      database: process.env.DATABASE
+      host: process.env.TITAN_API_HOST,
+      user: process.env.TITAN_API_USERNAME,
+      password: process.env.TITAN_API_PASSWORD,
+      database: process.env.TITAN_API_DATABASE
     }
 }
