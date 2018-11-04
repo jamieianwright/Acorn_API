740f4602de7f4a9274166620d5696edf06bfc847
jamie wright <https://jamieianwright@bitbucket.org/jamieianwright/>
2018-11-04 16:00:53 +0000

    sorting migration corruption

--- migrations/20181020162904_create_suppliers_table.js
+++ migrations/20181020162904_create_suppliers_table.js
@@ -11,5 +11,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('suppliers')
+    return knex.schema.dropTableIfExists('supplier')
 };
