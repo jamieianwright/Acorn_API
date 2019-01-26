9011d261c21316475d3ea26496c9d00f1134c92c
jamie wright <https://jamieianwright@bitbucket.org/jamieianwright/>
2019-01-26 14:44:31 +0000

    Project model and update to Component model

--- models/component.js
+++ models/component.js
@@ -1,10 +1,14 @@
 const db = require('../database');
 require('./supplier');
+require('./project');
 
 var Component = db.Model.extend({
   tableName: 'components',
   supplier: function() {
     return this.belongsTo('Supplier');
+  },
+  projects: function() {
+    return this.belongsToMany('Project', 'projects_components');
   }
 });
 
---
+++ models/project.js
@@ -0,0 +1,11 @@
+const db = require('../database');
+require('./component');
+
+var Project = db.Model.extend({
+    tableName: 'projects',
+    components: function() {
+      return this.belongsToMany('Component', 'projects_components');
+    }
+  });
+  
+  module.exports = db.model('Project', Project);
