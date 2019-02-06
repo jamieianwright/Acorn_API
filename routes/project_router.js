2aeacb56847684fc3bfb24100bb292a34e37b702
jamie wright <https://jamieianwright@bitbucket.org/jamieianwright/>
2019-02-06 18:50:01 +0000

    Added project component PUT and DELETE functionality

--- routes/project_router.js
+++ routes/project_router.js
@@ -1,22 +1,19 @@
 const express = require("express");
-const router = express.Router({ mergeParams: true });
+const router = express.Router({mergeParams: true});
 const Project = require('../models/project');
 
 router.get('/', (req, res) => {
     Project
         .where('projects.is_deleted', 0)
         .query(function (qb) {
-            qb.where(`${req.query.searchBy || 'projects.name'}`, 'LIKE', `%${req.query.search || ''}%`).orderBy(req.query.orderBy || 'name', req.query.order || 'asc')
+            qb
+                .where(`${req.query.searchBy || 'projects.name'}`, 'LIKE', `%${req.query.search || ''}%`)
+                .orderBy(req.query.orderBy || 'name', req.query.order || 'asc')
         })
         .fetchPage({
             page: req.query.page || 1,
             pageSize: (req.query.pageSize || 10),
-            columns: [
-                'projects.id',
-                'projects.name',
-                'description',
-                'is_active',
-            ]
+            columns: ['projects.id', 'projects.name', 'description', 'is_active']
         })
         .then(projects => {
             const responseObject = {
@@ -36,12 +33,13 @@ router.get('/:id', (req, res) => {
     let componentsOffset = componentsPageSize * (componentsPage - 1)
 
     Project
-        .forge({ id: req.params.id })
+        .forge({id: req.params.id})
         .fetch({
             withRelated: [
                 {
                     'components': function (qb) {
-                        qb.column('id', 'name', 'price', 'description', 'lead_time', 'min_order_quantity', 'supplier_id')
+                        qb
+                            .column('id', 'name', 'price', 'description', 'lead_time', 'min_order_quantity', 'supplier_id')
                             .orderBy(req.query.componentOrderBy || 'name', req.query.componentOrder || 'asc')
                             .limit(componentsPageSize)
                             .offset(componentsOffset);
@@ -51,23 +49,20 @@ router.get('/:id', (req, res) => {
                     }
                 }
             ],
-            columns: [
-                'id',
-                'name',
-                'description',
-                'is_active'
-            ]
+            columns: ['id', 'name', 'description', 'is_active']
         })
         .then(projects => {
             projects = projects.toJSON();
 
             if (projects.components.length > 0) {
-                projects.components.forEach((component, i) => {
-                    delete component._pivot_project_id;
-                    delete component._pivot_component_id;
-                    component.quantity = component._pivot_quantity
-                    delete component._pivot_quantity;
-                })
+                projects
+                    .components
+                    .forEach((component, i) => {
+                        delete component._pivot_project_id;
+                        delete component._pivot_component_id;
+                        component.quantity = component._pivot_quantity
+                        delete component._pivot_quantity;
+                    })
 
                 projects.componentsPagination = projects.componentsPagination[0];
                 projects.componentsPagination.page = componentsPage;
@@ -115,7 +110,7 @@ router.put('/:id', (req, res) => {
     } else {
         Project
             .where('id', req.params.id)
-            .save(req.body, { patch: true })
+            .save(req.body, {patch: true})
             .then(saved => res.json(saved))
             .catch((err) => res.status(500).send(err))
     }
@@ -125,7 +120,8 @@ router.put('/:id/component', (req, res) => {
     Project
         .where('id', req.params.id)
         .fetch()
-        .then(project => project.components().updatePivot(req.body))
+        .then(project => project.components().detach(req.body.component_id))
+        .then(project => project.attach(req.body))
         .then(project => res.json(project))
         .catch((err) => res.status(500).send(err))
 })
@@ -133,8 +129,19 @@ router.put('/:id/component', (req, res) => {
 router.delete('/:id', (req, res) => {
     Project
         .where('id', req.params.id)
-        .save({ is_deleted: '1' }, { patch: true })
+        .save({
+            is_deleted: '1'
+        }, {patch: true})
         .then(saved => res.status(200).send('Project deleted'))
+        .catch((err) => res.status(500).send(err))
+})
+
+router.delete('/:id/component', (req, res) => {
+    Project
+        .where('id', req.params.id)
+        .fetch()
+        .then(project => project.components().detach(req.body.component_id))
+        .then(project => res.json(project))
         .catch((err) => res.status(500).send(err))
 })
 
